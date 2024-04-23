e63024de31de7419e976199ffa56099886dc10b0
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2024-04-23 00:42:28 +0000

    Japanese localization

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => '入力内容と一致する登録情報がありません。',
+    'password' => 'パスワードに誤りがあります。',
+    'throttle' => 'ログインの試行回数が多すぎます。:seconds 秒後にお試しください。',
+
+];
