181a22e46dc661c72f4340c017f10f3fc4961bc2
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2024-04-23 07:08:54 +0000

    posts resource controller

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -12,9 +12,15 @@ class PostsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct(){
+        $this->middleware('auth:admin');
+    }
+
+
     public function index()
     {
-        //
+        dd('投稿一覧');
     }
 
     /**
--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -17,6 +17,9 @@
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('admin.posts.index')" :active="request()->routeIs('admin.posts.index')">
+                        投稿一覧
+                    </x-nav-link>
                 </div>
             </div>
 
