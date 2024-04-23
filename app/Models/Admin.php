6e4d3b140e300089829ff3416948ff01720d2667
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2024-04-23 08:30:23 +0000

    Admin Post relation

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -37,4 +37,9 @@ class Admin extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function post()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
