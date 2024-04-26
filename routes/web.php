4f1a88e8a9a88967266ad92b53fdae7931579e73
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2024-04-26 08:34:14 +0000

    general route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('user.welcome');
+// Route::get('/', function () {
+//     return view('user.welcome');
+// });
+
+Route::prefix('posts')
+->group(function() {
+    Route::get('index', [PostsController::class, 'postsIndex'])->name('posts.index');
+    Route::get('show/{show}', [PostsController::class, 'postsShow'])->name('posts.show');
 });
 
 Route::get('/dashboard', function () {
