e004e03248782eb63e4fd25adaae98f6c5f24dd8
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2024-04-27 01:05:49 +0000

    generalIndex modified

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -71,11 +71,6 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font">
                         <div class="flex">
-                            @if($post->is_visible)
-                                <span class="p-2 rounded-md bg-blue-400 text-white">表示中</span>
-                            @else
-                                <span class="p-2 rounded-md bg-red-400 text-white">非表示</span>
-                            @endif
                             @if($post->for_user)
                                 <span class="ml-3 p-2 rounded-md bg-yellow-600 text-white">会員限定</span>
                             @endif
